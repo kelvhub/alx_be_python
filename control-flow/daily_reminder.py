412bc48d2480a1a3f17a8753cf1bd887606fcb7f
noelkel <noelkelechivincent@gmail.com>
2025-06-01 00:46:28 +0100

    added

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 task = input("Enter your task:")
-time_bound = input("is it time-bound? (yes/no):")
-priority = input("priority (high/ medium/ low):")
+time_bound = input("is it time-bound? (yes/no):").lower()
+priority = input("priority (high/ medium/ low):").lower()
 
 
 match priority:
